631410d1bd4882d504bccb65aaf08a70dec7553b
Ivan Mikhailov <imihaylov@anna.money>
2019-07-10 13:47:57 +0300

    Add multihost property and update bool property

--- magic_settings/special_property.py
+++ magic_settings/special_property.py
@@ -20,10 +20,32 @@ class FloatProperty(Property):
 
 
 class BoolProperty(Property):
+    """
+    Returns 0 if value equals 'false', returns 1 if value equals 'true' (register-independent).
+    In other values, just returns init value
+    """
+    @staticmethod
+    def convert_bool_value_by_string(value: str):
+        if value.lower() == 'false':
+            return 0
+        if value.lower() == 'true':
+            return 1
+        return value
+
     def __init__(self, **kwargs):
-        super().__init__(**kwargs, types=bool, converts=[int, bool])
+        super().__init__(**kwargs, types=bool, converts=[self.convert_bool_value_by_string, int, bool])
 
 
 class StringListProperty(Property):
     def __init__(self, delimiter=',', **kwargs):
         super().__init__(**kwargs, types=list, converts=[partial(str.split, sep=delimiter)])
+
+
+class HostListProperty(Property):
+    @staticmethod
+    def split_hosts(hosts):
+        """Parse comma-separated host-port pairs. """
+        return [[host.split(':')[0], int(host.split(':')[1])] for host in hosts.split(',')]
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs, types=list, converts=[self.split_hosts])
--- tests/test_special_fields.py
+++ tests/test_special_fields.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
 
-from magic_settings.special_property import BoolProperty, FloatProperty, IntProperty, StringListProperty, StringProperty
+from magic_settings.special_property import (BoolProperty, FloatProperty, IntProperty, StringListProperty,
+                                             StringProperty, HostListProperty)
 from magic_settings.utils import BaseSettings
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -12,16 +13,20 @@ class TestSettings(BaseSettings):
     INTEGER_PROPERTY = IntProperty()
     FIRST_STRING_PROPERTY = StringProperty()
     SECOND_STRING_PROPERTY = StringProperty()
-    BOOL_TRUE_PROPERTY = BoolProperty()
-    BOOL_FALSE_PROPERTY = BoolProperty()
+    BOOL_NUMERIC_TRUE_PROPERTY = BoolProperty()
+    BOOL_NUMERIC_FALSE_PROPERTY = BoolProperty()
+    BOOL_WORD_TRUE_PROPERTY = BoolProperty()
+    BOOL_WORD_FALSE_PROPERTY = BoolProperty()
     FIRST_FLOAT_PROPERTY = FloatProperty()
     SECOND_FLOAT_PROPERTY = FloatProperty()
     DELIMETED_BY_COMMA_LIST_PROPERTY = StringListProperty()
     DELIMITED_BY_COLON_LIST_PROPERTY = StringListProperty(delimiter=':')
+    MULTI_HOST_PROPERTY = HostListProperty()
     MISSING_INTEGER_PROPERTY = IntProperty(default=8)
     MISSING_STRING_PROPERTY = StringProperty(default='Test string')
     MISSING_BOOL_PROPERTY = BoolProperty(default=True)
     MISSING_STRING_LIST_PROPERTY = StringListProperty(default=['test', 'test'])
+    MISSING_MULTI_HOST_PROPERTY = HostListProperty(default=[['localhost', 1234]])
 
 
 def test_init_settings():
@@ -31,8 +36,10 @@ def test_init_settings():
     assert settings.INTEGER_PROPERTY == 5
     assert settings.FIRST_STRING_PROPERTY == '1'
     assert settings.SECOND_STRING_PROPERTY == 'value'
-    assert settings.BOOL_TRUE_PROPERTY is True
-    assert settings.BOOL_FALSE_PROPERTY is False
+    assert settings.BOOL_NUMERIC_TRUE_PROPERTY is True
+    assert settings.BOOL_NUMERIC_FALSE_PROPERTY is False
+    assert settings.BOOL_WORD_TRUE_PROPERTY is True
+    assert settings.BOOL_WORD_FALSE_PROPERTY is False
     assert settings.FIRST_FLOAT_PROPERTY == 5.0
     assert settings.SECOND_FLOAT_PROPERTY == 5.5
     assert settings.DELIMETED_BY_COMMA_LIST_PROPERTY == ['one', 'two', 'three']
@@ -41,3 +48,5 @@ def test_init_settings():
     assert settings.MISSING_STRING_PROPERTY == 'Test string'
     assert settings.MISSING_BOOL_PROPERTY is True
     assert settings.MISSING_STRING_LIST_PROPERTY == ['test', 'test']
+    assert settings.MULTI_HOST_PROPERTY == [['192.168.20.1', 80], ['www.yandex.ru', 1234], ['localhost', 8888]]
+    assert settings.MISSING_MULTI_HOST_PROPERTY == [['localhost', 1234]]
